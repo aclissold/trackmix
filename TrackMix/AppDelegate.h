71b33464a282d42ced1b7abc46dc7ff4d25c189e
Andrew Clissold <andrewclissold@gmail.com>
2015-06-21 10:22:32 -0400

    Initial commit

---
+++ TrackMix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrackMix
+//
+//  Created by Andrew Clissold on 6/21/15.
+//  Copyright (c) 2015 Andrew Clissold. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TrackMix/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TrackMix
+//
+//  Created by Andrew Clissold on 6/21/15.
+//  Copyright (c) 2015 Andrew Clissold. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
