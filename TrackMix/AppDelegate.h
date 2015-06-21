4964a4924edba17d997c8b3f307ab1ea57d4dca4
Andrew Clissold <andrewclissold@gmail.com>
2015-06-21 10:51:56 -0400

    Add outlets

--- TrackMix/AppDelegate.h
+++ TrackMix/AppDelegate.h
@@ -10,6 +10,9 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
+@property (weak) IBOutlet NSTextField *textField;
+@property (weak) IBOutlet NSSlider *slider;
+
 - (IBAction)mute:(id)sender;
 - (IBAction)takeFloatValueForVolumeFrom:(id)sender;
 
