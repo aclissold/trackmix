0f9f36d583a7e862fc171bb22a7cb903377ad1fc
Andrew Clissold <andrewclissold@gmail.com>
2015-06-21 10:55:28 -0400

    Generate Track class

---
+++ TrackMix/Track.h
@@ -0,0 +1,13 @@
+//
+//  Track.h
+//  TrackMix
+//
+//  Created by Andrew Clissold on 6/21/15.
+//  Copyright (c) 2015 Andrew Clissold. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Track : NSObject
+
+@end
---
+++ TrackMix/Track.m
@@ -0,0 +1,13 @@
+//
+//  Track.m
+//  TrackMix
+//
+//  Created by Andrew Clissold on 6/21/15.
+//  Copyright (c) 2015 Andrew Clissold. All rights reserved.
+//
+
+#import "Track.h"
+
+@implementation Track
+
+@end
